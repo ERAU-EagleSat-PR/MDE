40160e8302f09ec3407347daff6803101c050e78
nivlac3213 <80916176+Nivlac3213@users.noreply.github.com>
2022-06-17 22:45:27 -0500

    Defiend CS ports and pins, hex values for multiplexed combos, funsion prototype for mux logic

--- MDE_Payload_Prototype_TM4C1294/Access_Tools.h
+++ MDE_Payload_Prototype_TM4C1294/Access_Tools.h
@@ -12,7 +12,7 @@
 // to this, it's going to be the total number of chips minus 1. Only changing
 // this does not handle higher numbers of chips. Changes must also be made to
 // the retrieve ports and pins functions.
-#define MAX_CHIP_NUMBER 39
+#define MAX_CHIP_NUMBER 31
 
 // Method prototypes
 extern void WriteToChip(uint32_t chip_number, unsigned char sequence_start, unsigned char sequence_offset);
@@ -22,141 +22,29 @@ extern void EnableChipSelects();
 // GPIO Ports to enable for chip selects, hard-coded to enable each in the
 // EnableChipSelects method. If another port needs to be enabled, add a macro
 // here and enable in that method.
-#define CS_SYSCTL_PORT_0 SYSCTL_PERIPH_GPIOM
-#define CS_SYSCTL_PORT_1 SYSCTL_PERIPH_GPIOH
-#define CS_SYSCTL_PORT_2 SYSCTL_PERIPH_GPIOD
-#define CS_SYSCTL_PORT_3 SYSCTL_PERIPH_GPION
-#define CS_SYSCTL_PORT_4 SYSCTL_PERIPH_GPIOP
-#define CS_SYSCTL_PORT_5 SYSCTL_PERIPH_GPIOF
 
-// Generally, two pieces of information are needed for each chip: a pin and
-// a port for the chip select. Each chip also has a number, but the relation
-// between these macros and those numbers is hardcoded in the Access_Tools.c
-// file. This is an awful solution, but the best that I could think of.
+// Chip Select ports and pins for primary memory board
+#define CS0_0_PORT GPIO_PORTD_BASE
+#define CS0_0_PIN GPIO_PIN_0
+#define CS0_1_PORT GPIO_PORTD_BASE
+#define CS0_1_PIN GPIO_PIN_1
+#define CS0_2_PORT GPIO_PORTD_BASE
+#define CS0_2_PIN GPIO_PIN_2
+#define CS0_3_PORT GPIO_PORTD_BASE
+#define CS0_3_PIN GPIO_PIN_3
 
-// EEPROM Mapping - chips 0 through 7
-#define EEPROM_PORT_0 GPIO_PORTN_BASE
-#define EEPROM_PIN_0 GPIO_PIN_3
 
-#define EEPROM_PORT_1 GPIO_PORTP_BASE
-#define EEPROM_PIN_1 GPIO_PIN_2
+// Chip Select ports and pins for 2nd memory board
+#define CS1_0_PORT GPIO_PORTC_BASE
+#define CS1_0_PIN GPIO_PIN_6
+#define CS1_1_PORT GPIO_PORTC_BASE
+#define CS1_1_PIN GPIO_PIN_7
+#define CS1_2_PORT GPIO_PORTD_BASE
+#define CS1_2_PIN GPIO_PIN_6
+#define CS1_3_PORT GPIO_PORTD_BASE
+#define CS1_3_PIN GPIO_PIN_7
 
-#define EEPROM_PORT_2 0
-#define EEPROM_PIN_2 0
-
-#define EEPROM_PORT_3 0
-#define EEPROM_PIN_3 0
-
-#define EEPROM_PORT_4 0
-#define EEPROM_PIN_4 0
-
-#define EEPROM_PORT_5 0
-#define EEPROM_PIN_5 0
-
-#define EEPROM_PORT_6 0
-#define EEPROM_PIN_6 0
-
-#define EEPROM_PORT_7 0
-#define EEPROM_PIN_7 0
-
-// FLASH Mapping - chips 8 through 15
-#define FLASH_PORT_0 GPIO_PORTM_BASE
-#define FLASH_PIN_0 GPIO_PIN_3
-
-#define FLASH_PORT_1 GPIO_PORTH_BASE
-#define FLASH_PIN_1 GPIO_PIN_2
-
-#define FLASH_PORT_2 0
-#define FLASH_PIN_2 0
-
-#define FLASH_PORT_3 0
-#define FLASH_PIN_3 0
-
-#define FLASH_PORT_4 0
-#define FLASH_PIN_4 0
-
-#define FLASH_PORT_5 0
-#define FLASH_PIN_5 0
-
-#define FLASH_PORT_6 0
-#define FLASH_PIN_6 0
-
-#define FLASH_PORT_7 0
-#define FLASH_PIN_7 0
-
-// FRAM Mapping - chips 16 through 23
-#define FRAM_PORT_0 GPIO_PORTF_BASE
-#define FRAM_PIN_0 GPIO_PIN_1
-
-#define FRAM_PORT_1 GPIO_PORTF_BASE
-#define FRAM_PIN_1 GPIO_PIN_2
-
-#define FRAM_PORT_2 0
-#define FRAM_PIN_2 0
-
-#define FRAM_PORT_3 0
-#define FRAM_PIN_3 0
-
-#define FRAM_PORT_4 0
-#define FRAM_PIN_4 0
-
-#define FRAM_PORT_5 0
-#define FRAM_PIN_5 0
-
-#define FRAM_PORT_6 0
-#define FRAM_PIN_6 0
-
-#define FRAM_PORT_7 0
-#define FRAM_PIN_7 0
-
-// MRAM Mapping - chips 24 through 31
-#define MRAM_PORT_0 GPIO_PORTH_BASE
-#define MRAM_PIN_0 GPIO_PIN_3
-
-#define MRAM_PORT_1 GPIO_PORTD_BASE
-#define MRAM_PIN_1 GPIO_PIN_1
-
-#define MRAM_PORT_2 0
-#define MRAM_PIN_2 0
-
-#define MRAM_PORT_3 0
-#define MRAM_PIN_3 0
-
-#define MRAM_PORT_4 0
-#define MRAM_PIN_4 0
-
-#define MRAM_PORT_5 0
-#define MRAM_PIN_5 0
-
-#define MRAM_PORT_6 0
-#define MRAM_PIN_6 0
-
-#define MRAM_PORT_7 0
-#define MRAM_PIN_7 0
-
-// SRAM Mapping - chips 32 through 39
-#define SRAM_PORT_0 GPIO_PORTD_BASE
-#define SRAM_PIN_0 GPIO_PIN_0
-
-#define SRAM_PORT_1 GPIO_PORTN_BASE
-#define SRAM_PIN_1 GPIO_PIN_2
-
-#define SRAM_PORT_2 0
-#define SRAM_PIN_2 0
-
-#define SRAM_PORT_3 0
-#define SRAM_PIN_3 0
-
-#define SRAM_PORT_4 0
-#define SRAM_PIN_4 0
-
-#define SRAM_PORT_5 0
-#define SRAM_PIN_5 0
-
-#define SRAM_PORT_6 0
-#define SRAM_PIN_6 0
-
-#define SRAM_PORT_7 0
-#define SRAM_PIN_7 0
+uint8_t CHIP_SELECT_COMBOS[16] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                                   0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
 
 #endif
