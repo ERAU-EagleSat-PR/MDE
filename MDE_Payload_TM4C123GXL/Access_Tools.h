a81efbc2aeafbd6b3fdb5e586e11758bf4f6ccdf
Tyler Thurman <tylerthurman09@gmail.com>
2022-10-17 23:01:44 -0700

    MRAM enable, h file organization
    Moved some definitions from MDE_Mux_Prototype.h to Access_Tools.h for organization and to remove multiple duplicates. Rewrote the MRAM read and write functions to function with the new chip select methods. Did not touch the actual reading or writing sections themselves.

--- MDE_Payload_TM4C123GXL/Access_Tools.h
+++ MDE_Payload_TM4C123GXL/Access_Tools.h
@@ -12,7 +12,7 @@
 // to this, it's going to be the (total number of chips) - 1. Only changing
 // this does not handle higher numbers of chips. Changes must also be made to
 // the retrieve ports and pins functions.
-#define MAX_CHIP_NUMBER 31
+#define MAX_CHIP_NUMBER 15
 
 // Method prototypes
 extern void WriteToChip(uint32_t chip_number, unsigned char sequence_start, unsigned char sequence_offset);
@@ -23,22 +23,47 @@ extern void EnableChipSelects();
 // EnableChipSelects method. If another port needs to be enabled, add a macro
 // here and enable in that method.
 
+// SSI pins for MOSI/MISO board communication
+// SSI pins and ports Board 1
+#define SPI0_NUM_BASE SSI0_BASE
+#define SPI0_SYS_PERIPH SYSCTL_PERIPH_SSI0
+#define SPI0_SYS_PORT SYSCTL_PERIPH_GPIOA
+#define SPI0_PORT GPIO_PORTA_BASE
+#define SPI0_CLK GPIO_PA2_SSI0CLK
+#define SPI0_CLK_NUM GPIO_PIN_2
+#define SPI0_MOSI GPIO_PA5_SSI0XDAT0
+#define SPI0_MISO GPIO_PA4_SSI0XDAT1
+#define SPI0_MOSI_NUM GPIO_PIN_5
+#define SPI0_MISO_NUM GPIO_PIN_4
+
+// SSI pins and ports Board 2
+#define SPI1_NUM_BASE SSI1_BASE
+#define SPI1_SYS_PERIPH SYSCTL_PERIPH_SSI1
+#define SPI1_SYS_PORT SYSCTL_PERIPH_GPIOF
+#define SPI1_PORT GPIO_PORTF_BASE
+#define SPI1_CLK GPIO_PF2_SSI1CLK
+#define SPI1_CLK_NUM GPIO_PIN_2
+#define SPI1_MOSI GPIO_PF1_SSI1XDAT0
+#define SPI1_MISO GPIO_PF0_SSI1XDAT1
+#define SPI1_MOSI_NUM GPIO_PIN_1
+#define SPI1_MISO_NUM GPIO_PIN_0
+
 // Chip Select ports and pins for primary memory board
-// All pins are on the same port
-#define BOARD1_CS_PORT  GPIO_PORTD_BASE
-#define CS0_0_PIN   GPIO_PIN_0
-#define CS0_1_PIN   GPIO_PIN_1
-#define CS0_2_PIN   GPIO_PIN_2
-#define CS0_3_PIN   GPIO_PIN_3
+// Chip Select Pins and Ports Board 1
+#define CS0_SYSCTL_PORT SYSCTL_PERIPH_GPIOD
+#define CS0_PORT  GPIO_PORTD_BASE
+#define CS0_PIN_0 GPIO_PIN_0
+#define CS0_PIN_1 GPIO_PIN_1
+#define CS0_PIN_2 GPIO_PIN_2
+#define CS0_PIN_3 GPIO_PIN_3
 
-
-// Chip Select ports and pins for 2nd memory board
-// All pins are on the same port
-#define BOARD2_CS_PORT  GPIO_PORTC_BASE
-#define CS1_0_PIN   GPIO_PIN_4
-#define CS1_1_PIN   GPIO_PIN_5
-#define CS1_2_PIN   GPIO_PIN_6
-#define CS1_3_PIN   GPIO_PIN_7
+// Chip Select Pins and Ports Board 2
+#define CS1_SYSCTL_PORT SYSCTL_PERIPH_GPIOC
+#define CS1_PORT  GPIO_PORTC_BASE
+#define CS1_PIN_0 GPIO_PIN_4
+#define CS1_PIN_1 GPIO_PIN_5
+#define CS1_PIN_2 GPIO_PIN_6
+#define CS1_PIN_3 GPIO_PIN_7
 
 
 // Multiplexer input for both boards
@@ -58,5 +83,9 @@ extern void EnableChipSelects();
 #define SRAM2_MUX_CS    0x0D
 #define SRAM3_MUX_CS    0x0E
 #define SRAM4_MUX_CS    0x0F
+
+// MUX chip select assignments for looping
+uint8_t CHIP_SELECT_MUX_PINS[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+
 
 #endif
