17b39c87b7e9994cf3879fa0d10e6e5e021f0f61
Nikhil Dave <nikhilrdave2003@gmail.com>
2023-11-01 18:32:42 -0700

    devtools: small changes/notes Note the RAND_MAX for why cell_address is only 16 bit Also make the error printing in the Error Queue menu nicer

--- source/devtools.c
+++ source/devtools.c
@@ -768,8 +768,6 @@ void processChipHealthInput(int32_t recv_char)
 void processErrorInput(int32_t recv_char)
 {
     IntMasterDisable();
-    //char buf[50];
-    //uint8_t bufSize = 50;
 
     switch (recv_char)
     {
@@ -783,6 +781,7 @@ void processErrorInput(int32_t recv_char)
         // to acceptable ranges for the argument - i.e. uint8_t arguments will only get values between 0 and 255
         // The byte number/memory address is uint32_t, and rand() returns an int that is guaranteed to be positive,
         // so we don't have to worry about doing anything to the value from rand().
+        // Note that RAND_MAX for the microcontroller is 32767, so the cell address is limited
         ErrorQueue_Insert(&errorHead, rand() % UINT8_MAX, rand(), rand() % UINT8_MAX, rand() % UINT8_MAX);
         current_error++;
         break;
@@ -796,6 +795,7 @@ void processErrorInput(int32_t recv_char)
         current_error = 0;
         break;
     case 'p': // Print the entire queue.
+        printDebugMenu();
         ErrorQueue_PrintLink(errorHead->next, current_error);
         break;
     }
